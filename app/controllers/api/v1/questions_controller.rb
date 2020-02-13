144325b6c0de4ff1a56651c7f6518d872c11e67e
amfosh <50755533+amfosh@users.noreply.github.com>
2020-02-13 08:31:11 -0500

    added controller functions

---
+++ app/controllers/api/v1/questions_controller.rb
@@ -0,0 +1,21 @@
+class Apt::V1::QuestionsController < ApplicationController
+
+    def index
+        @questions = Question.all
+
+        render json: @questions, status: 200
+    end
+
+    def create
+        @question = Question.new(question_params)
+        if @question.save
+            render json: @question, status: 200
+        end
+    end
+
+    private
+
+    def question_params
+        params.require(:question).permit(:ask, :dont_ask)
+    end
+end
