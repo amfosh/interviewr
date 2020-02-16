9ffb66bfcc7132ca56a4ae0ec40e6712a284ca62
amfosh <50755533+amfosh@users.noreply.github.com>
2020-02-16 12:32:31 -0500

    scope method added

--- backend/app/models/question.rb
+++ backend/app/models/question.rb
@@ -1,3 +1,13 @@
 class Question < ApplicationRecord
   belongs_to :user
+  scope :ask, -> { where(ask: true) }
+  scope :dont_ask, -> { where(dont_ask: true) }
+
+  # def self.ask 
+  #   where(ask: true)
+  # end
+
+  # def self.dont_ask 
+  #   where(dont_ask: true)
+  # end
 end
