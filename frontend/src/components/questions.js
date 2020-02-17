5874b3588573b79a6c24043a405cd2a524c1cddc
amfosh <50755533+amfosh@users.noreply.github.com>
2020-02-17 10:57:33 -0500

    questions showing up one by one upon load

--- frontend/src/components/questions.js
+++ frontend/src/components/questions.js
@@ -3,6 +3,7 @@ class Questions {
         this.questions = []
         this.adapter = new QuestionsAdapter()
         this.fetchAndLoadQuestions()
+        this.newQuestion()
     }
 
     fetchAndLoadQuestions(){
@@ -12,12 +13,40 @@ class Questions {
             questions.forEach(question => this.questions.push(new Question(question)))
         })
         .then(() => {
-            this.render()
+            this.newQuestion()
         })
     }
-
-    render() {
-        const questionDisplay = document.getElementById('questionDisplay')
-        questionDisplay.innerHTML = this.questions.map(question => `<li>${question.content}</li>`).join('')
+    
+    newQuestion() {
+        var randomNumber = Math.floor(Math.random() * (this.questions.length));
+        document.getElementById('questionDisplay').innerHTML = this.questions[randomNumber].content;
+        // console.log(this.questions[randomNumber])
     }
+//         if (questionNum >= questions.length) {
+//             alert("you've seen all questions");
+//             done = true;
+//         } else {
+//         document.getElementById("question").innerHTML = question[questionNum];
+//         }
+//     }
+
+// loadQuestion();
+
+// document.getElementById("next").addEventListener("click", function(e) {
+//     if (done) {
+//         return;
+//     }
+//     if (checkAnswer()) {
+//         ++numCorrect;
+//         document.getElementById("numCorrect").innerHTML = numCorrect;
+//         loadQuestion();
+//     } else {
+//         alert("Answer is not correct");
+//     }
+// });
+
+//     render() {
+//         const questionDisplay = document.getElementById('questionDisplay')
+//         questionDisplay.innerHTML = this.questions.map(question => `<li>${question.content}</li>`).join('')
+//     }
 }
